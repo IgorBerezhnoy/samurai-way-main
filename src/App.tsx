6633944e871b055ef7bd51ee7dd8ef57a1d831f2
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-04-29 23:40:15 +0800

    finished task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <ul>
+        <li>Html</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </ul>
     </div>
   );
 }
