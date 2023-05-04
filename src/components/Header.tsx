55700d841ddf988d152c9c3f757716fb272f91b6
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-04 17:56:44 +0800

    creaat css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+import "./Header.module.css"
+
 export const Header = () => {
-  return (
-    <header className="header">
-      <img
-        src="https://mir-s3-cdn-cf.behance.net/projects/max_808/54881128032875.5628e85cddaa7.jpg"
-        alt="Logotype"
-      />
-    </header>
-  );
+    return (
+        <header className="header">
+            <img
+                src="https://mir-s3-cdn-cf.behance.net/projects/max_808/54881128032875.5628e85cddaa7.jpg"
+                alt="Logotype"
+            />
+        </header>
+    );
 };
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,22 +1,24 @@
+import "./Profile.module.css"
+
 export const Profile = () => {
-  return (
-    <div className="content">
-      <div>
-        <img src="https://img3.goodfon.ru/original/1152x864/8/69/priroda-gory-kamni-les.jpg" />
-      </div>
-      <div>
-        ava+description
-        <img src="https://mir-s3-cdn-cf.behance.net/user/276/72bf191180636107.63ccd92aa95c0.png" />
-      </div>
-      <div>
-        My posts
-        <div>New post</div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
-          <div>post 3</div>
+    return (
+        <div className="content">
+            <div>
+                <img src="https://img3.goodfon.ru/original/1152x864/8/69/priroda-gory-kamni-les.jpg"/>
+            </div>
+            <div>
+                ava+description
+                <img src="https://mir-s3-cdn-cf.behance.net/user/276/72bf191180636107.63ccd92aa95c0.png"/>
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div className={"posts"}>
+                    <div className={"item"}>post 1</div>
+                    <div className={"item"}>post 2</div>
+                    <div className={"item"}>post 3</div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
