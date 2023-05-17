82d10be2059abf362c3dc8966730ea9ea8b7c045
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-05-18 02:27:00 +0800

    Revert "Finished lesson16"
    This reverts commit ed312eed107d478fcf5b9249347dbbbbd151c2c0.

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -12,7 +12,7 @@ export const Post = (props:PostPropsType) =>
             src="https://mir-s3-cdn-cf.behance.net/user/276/72bf191180636107.63ccd92aa95c0.png"/>
             {props.message}
             <div>
-                <span>like </span> {props.like}
+                <span>{props.like}</span>
             </div>
 
         </div>
